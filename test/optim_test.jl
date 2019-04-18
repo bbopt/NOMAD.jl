8bff1b30edf2b3ecba8802c2edd00c6087de3adf
ppascal97 <49170339+ppascal97@users.noreply.github.com>
2019-04-18 11:17:30 -0300

    added unitary tests

--- test/optim_test.jl
+++ test/optim_test.jl
@@ -1,8 +1,9 @@
-using NOMADjl
+#surrogates
+#valgrind ou équivalent : fuites de mémoire
+#juliasmoothoptimizers, NLPmodels, ex Documenter
 
-path_to_nomad = "/home/pascpier/Documents/nomad.3.9.1-copy"
-
-init(path_to_nomad)
+using NOMAD
+using Test
 
 function cost(x)
 	y=(x[1]+2*x[2]-7)^2+(2*x[1]+x[2]-5)^2
@@ -27,14 +28,6 @@ function eval1(x)
     return (count_eval,bb_outputs)
 end
 
-function eval1(x)
-    f=cost(x)
-    c=constraint2(x)
-    count_eval=true
-	bb_outputs = [f,c]
-    return (count_eval,bb_outputs)
-end
-
 function eval2(x)
     f=cost(x)
     c=constraint2(x)
@@ -43,22 +36,32 @@ function eval2(x)
     return (count_eval,bb_outputs)
 end
 
-param=NOMADjl.parameters()
-param.dimension=2
-param.output_types=["OBJ","EB"]
-param.display_stats="bbe ( sol ) obj"
-param.display_all_eval=false
-param.x0=[2,4]
-param.lower_bound=[-10,-10]
-param.upper_bound=[10,10]
-param.max_bb_eval=100
-param.display_degree=2
-param.solution_file="sol.txt"
+param1=NOMAD.parameters()
+param1.dimension=2
+param1.output_types=["OBJ","EB"]
+param1.display_stats="bbe ( sol ) obj"
+param1.display_all_eval=false
+param1.x0=[2,3]
+param1.max_bb_eval=100
+param1.display_degree=2
+param1.solution_file="sol.txt"
 
-result1=NOMADjl.runopt(eval1,param)
+param2=NOMAD.parameters(param1)
+param2.x0=[9,9]
+param2.max_bb_eval=120
+param2.display_degree=1
 
-result2=NOMADjl.runopt(eval2,param)
+result1 = NOMAD.runopt(eval1,param1)
+@test result1.success
+@test result1.best_feasible ≈ [1.0, 3.0]
+@test result1.bb_eval == param1.max_bb_eval
 
-param.x0=[9,9]
+result2 = NOMAD.runopt(eval2,param1)
+@test result2.success
+@test result2.best_feasible ≈ [2.0, 2.2]
+@test result2.bb_eval == param1.max_bb_eval
 
-result3=NOMADjl.runopt(eval2,param)
+result3 = NOMAD.runopt(eval2,param2)
+@test result3.success
+@test result3.best_feasible ≈ [2.0, 2.2]
+@test result3.bb_eval == param2.max_bb_eval
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,3 @@
-using NOMADjl
+using NOMAD
 
 include("optim_test.jl")
