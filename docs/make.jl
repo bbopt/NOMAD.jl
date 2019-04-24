583b9b190cb47ae48f419ca97b8cd8823582d667
ppascal97 <49170339+ppascal97@users.noreply.github.com>
2019-04-24 12:04:44 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,12 +3,6 @@ using Documenter, NOMAD
 makedocs(
 linkcheck = true,
 sitename = "NOMAD.jl",
-format = Documenter.HTML(
-           # Use clean URLs, unless built as a "local" build
-           prettyurls = !("local" in ARGS),
-           canonical = "https://juliadocs.github.io/Documenter.jl/stable/",
-           analytics = "UA-89508993-1",
-),
 pages = ["Home" => "index.md",
            "Parameters" => "parameters.md",
            "Run Optimization" => "runopt.md",
