893192e13818d3b79ef474394c666615518e03c9
Alexis Montoison <alexis.montoison@polymtl.ca>
2020-02-15 16:38:34 -0500

    Update documentation script

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,18 @@
 using Documenter, NOMAD
 
 makedocs(
-    linkcheck = true,
-    sitename = "NOMAD.jl",
-    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
-    pages = ["Home" => "index.md",
-               "Parameters" => "nomadParameters.md",
-               "Run Optimization" => "run_nomad.md",
-               "Results" => "nomadResults.md",
-               "Surrogates" => "surrogates.md"
-              ]
+  sitename = "NOMAD.jl",
+  modules = [NOMAD],
+  doctest = true,
+  linkcheck = true,
+  strict = true,
+  format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+  pages = ["Home" => "index.md",
+           "Parameters" => "nomadParameters.md",
+           "Run Optimization" => "run_nomad.md",
+           "Results" => "nomadResults.md",
+           "Surrogates" => "surrogates.md"
+          ]
 )
 
-deploydocs(
-    repo = "github.com/ppascal97/NOMAD.jl.git",
-    target = "build",
-)
+deploydocs(repo = "github.com/amontoison/NOMAD.jl.git")
