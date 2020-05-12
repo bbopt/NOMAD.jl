25250b0e9e958b538260cbd4f9b7576b65d54f4f
Alexis Montoison <alexis.montoison@polymtl.ca>
2020-05-11 22:50:06 -0400

    Check compilation of NOMAD 3.9.1 on Linux and Mac

--- deps/build.jl
+++ deps/build.jl
@@ -2,23 +2,23 @@ builddir = @__DIR__
 nomad_path = joinpath(builddir,"nomad.3.9.1")
 
 if ispath(nomad_path)
-	error("NOMAD.jl building error : nomad folder already exists, first remove it before building anew")
+  error("NOMAD.jl building error : nomad folder already exists, first remove it before building anew")
 end
 
 if Sys.iswindows()
-	error("NOMAD.jl error : The package is not compatible with Windows operating systems")
+  error("NOMAD.jl error : The package is not compatible with Windows operating systems")
 else
-	# We will have NOMAD compiled on your machine
-	try
-		zipnomad = joinpath(builddir,"downloads/NOMAD.zip")
-		run(`unzip $zipnomad -d $builddir`)
-		ENV["NOMAD_HOME"] = nomad_path
-		cd(nomad_path)
-		run(`./configure`)
-		run(`make`)
-	catch e
-		rm(nomad_path; recursive=true, force=true)
-		@warn "NOMAD could not be compiled automatically, try ./configure and make extracted files from NOMAD.jl/deps/downloads/NOMAD.zip"
-		throw(e)
-	end
+  # We will have NOMAD compiled on your machine
+  try
+    zipnomad = joinpath(builddir,"downloads/NOMAD.zip")
+    run(`unzip $zipnomad -d $builddir`)
+    ENV["NOMAD_HOME"] = nomad_path
+    cd(nomad_path)
+    run(`./configure`)
+    run(`make`)
+  catch e
+    rm(nomad_path; recursive=true, force=true)
+    @warn "NOMAD could not be compiled automatically, try ./configure and make extracted files from NOMAD.jl/deps/downloads/NOMAD.zip"
+    throw(e)
+  end
 end
