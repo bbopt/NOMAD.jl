672558791145b3b3392028685cdae81c313a14b7
salomonl <ludovic.salomon@gerad.ca>
2021-01-18 18:15:17 -0500

    Add linear constraints for NOMAD.jl

--- src/core.jl
+++ src/core.jl
@@ -13,9 +13,13 @@ const BBOutputTypes = ["OBJ" # objective type
                        "PB"  # progressive barrier constraint
                       ]
 
+const BBLinearConverterTypes = ["QR" # QR Linear converter
+                                "SVD" # SVD Linear converter
+                               ]
+
 mutable struct NomadOptions
 
-    # display otions
+    # display options
     display_degree::Int # display degree: between 0 and 3
     display_all_eval::Bool # display all evaluations
     display_infeasible::Bool # display infeasible
@@ -31,6 +35,10 @@ mutable struct NomadOptions
     speculative_search::Bool
     nm_search::Bool
 
+    # linear constrained optimization options
+    linear_converter::String
+    linear_constraints_atol::Float64
+
     function NomadOptions(;display_degree::Int = 2,
                           display_all_eval::Bool = false,
                           display_infeasible::Bool = false,
@@ -40,7 +48,9 @@ mutable struct NomadOptions
                           use_cache::Bool = true,
                           lh_search::Tuple{Int, Int} =(0,0),
                           speculative_search::Bool=true,
-                          nm_search::Bool=true)
+                          nm_search::Bool=true,
+                          linear_converter::String="SVD",
+                          linear_constraints_atol::Float64=0.0)
         return new(display_degree,
                    display_all_eval,
                    display_infeasible,
@@ -50,7 +60,9 @@ mutable struct NomadOptions
                    use_cache,
                    lh_search,
                    speculative_search,
-                   nm_search)
+                   nm_search,
+                   linear_converter,
+                   linear_constraints_atol)
     end
 end
 
@@ -58,6 +70,8 @@ function check_options(options::NomadOptions)
     (0 <= options.display_degree <= 3) ? nothing : error("Nomad.jl error: display_degree must be comprised between 0 and 3")
     (options.max_bb_eval > 0) ? nothing : error("NOMAD.jl error: wrong parameters, max_bb_eval must be strictly positive")
     (options.lh_search[1] >= 0 && options.lh_search[2] >=0) ? nothing : error("NOMAD.jl error: the lh_search parameters must be positive or null")
+    (options.linear_converter in BBLinearConverterTypes) ? nothing : error("NOMAD.jl error: the linear_converter type is not defined")
+    (options.linear_constraints_atol >= 0) ? nothing : error("NOMAD.jl error: the linear_constraints_atol parameter must be positive or null")
 end
 
 """
@@ -221,6 +235,12 @@ struct NomadProblem
 
     output_types::Vector{String}
 
+    # optional matrix A
+    A::Union{Nothing, Array{Float64, 2}}
+
+    # optional vector b
+    b::Union{Nothing, Vector{Float64}}
+
     # callback
     eval_bb::Function
 
@@ -234,7 +254,9 @@ struct NomadProblem
                           input_types::Vector{String} = ["R" for i in 1:nb_inputs],
                           granularity::Vector{Float64} = zeros(Float64, nb_inputs),
                           lower_bound::Vector{Float64} = -Inf * ones(Float64, nb_inputs),
-                          upper_bound::Vector{Float64} = Inf * ones(Float64, nb_inputs))
+                          upper_bound::Vector{Float64} = Inf * ones(Float64, nb_inputs),
+                          A::Union{Nothing, Array{Float64, 2}} = nothing,
+                          b::Union{Nothing, Vector{Float64}} = nothing)
 
         @assert nb_inputs > 0 "NOMAD.jl error : wrong parameters, the number of inputs must be strictly positive"
         @assert nb_inputs == length(lower_bound) "NOMAD.jl error: wrong parameters, lower bound is not consistent with the number of inputs"
@@ -245,9 +267,19 @@ struct NomadProblem
         @assert nb_outputs > 0 "NOMAD.jl error : wrong parameters, the number of outputs must be strictly positive"
         @assert nb_outputs == length(output_types) "NOMAD.jl error : wrong parameters, output types is not consistent with the number of outputs"
 
+        if (A != nothing) || (b != nothing)
+            @assert A != nothing && b != nothing "NOMAD.jl error: wrong parameter, A and b must be initialized together"
+        end
+
+        if A != nothing
+            @assert size(A, 2) == nb_inputs "NOMAD.jl error: wrong parameters, dimensions of A is not consistent with the number of inputs"
+            @assert size(A, 1) == length(b) "NOMAD.jl error: wrong parameters, dimensions of A are not consistent with dimensions of b"
+            @assert size(A, 1) < size(A, 2) "NOMAD.jl error: wrong parameters, A ∈ Rᵐˣⁿ must satisfy m < n"
+        end
+
         return new(nb_inputs, nb_outputs, input_types,
                    granularity, lower_bound, upper_bound,
-                   output_types, eval_bb, NomadOptions())
+                   output_types, A, b, eval_bb, NomadOptions())
     end
 end
 
@@ -260,9 +292,17 @@ function check_problem(p::NomadProblem)
 
     for i in 1:p.nb_inputs
         if p.input_types[i] == "R"
-            p.granularity[i] >= 0 || error("NOMAD.jl error:  wrong parameters, $(i)th coordinate of granularity is negative")
+            p.granularity[i] >= 0 || error("NOMAD.jl error: wrong parameters, $(i)th coordinate of granularity is negative")
         elseif p.input_types[i] in ["I","B"]
-            p.granularity[i] in [0,1] || error("NOMAD.jl error : $(i)th coordinate of granularity is automatically set to 1")
+            p.granularity[i] in [0,1] || error("NOMAD.jl error: $(i)th coordinate of granularity is automatically set to 1")
+        end
+    end
+
+    # The resolution of a linear-constrained blackbox problem requires real and non granular variables.
+    if p.A != nothing
+        all(p.input_types .== "R") || error("NOMAD.jl error: wrong parameters, all blackbox inputs must be real when solving a linear constrained blackbox optimization problem")
+        for i in 1:p.nb_inputs
+            p.granularity[i] == 0 || error("NOMAD.jl error: wrong parameters, $(i)th coordinate of granularity must be set to 0 when solving a linear constrained blackbox optimization problem") 
         end
     end
 
@@ -318,16 +358,64 @@ function solve(p::NomadProblem, x0::Vector{Float64})
     check_options(p.options)
     @assert p.nb_inputs == length(x0) "NOMAD.jl error : wrong parameters, starting point size is not consistent with bb inputs"
 
+    # verify starting point satisfies approximately the linear constraints 
+    if p.A != nothing
+        isapprox(p.A * x0, p.b, atol=p.options.linear_constraints_atol) || error("NOMAD.jl error : starting point x0 does does not satisfy the linear constraints")
+    end
+
     # 2- create c_nomad_problem
-    input_types_wrapper = "( " * join(p.input_types, " ") * " )"
-    output_types_wrapper = join(p.output_types, " ")
+    # identity converter in case there are no linear constraints
+    converter = identity
 
-    c_nomad_problem = create_c_nomad_problem(p.eval_bb, p.nb_inputs, p.nb_outputs,
-                                             p.lower_bound, p.upper_bound,
-                                             input_types_wrapper, output_types_wrapper,
-                                             p.options.max_bb_eval)
+    # TODO refactorize
+    c_nomad_problem = begin
+        if p.A == nothing # no linear constraints
+            input_types_wrapper = "( " * join(p.input_types, " ") * " )"
+            output_types_wrapper = join(p.output_types, " ")
+            create_c_nomad_problem(p.eval_bb ∘ converter, p.nb_inputs, p.nb_outputs,
+                                   p.lower_bound, p.upper_bound,
+                                   input_types_wrapper, output_types_wrapper,
+                                   p.options.max_bb_eval)
+        else # linear constraints
+            # TODO to change with new converters
+            converter = p.options.linear_converter == "SVD" ? SVDConverter(p.A, p.b) : QRConverter(p.A, p.b)
 
-    # 3 - set options
+            nz = get_nz_dimension(converter)
+
+            # fix blackbox new dimensions
+            input_types_wrapper = "( " * join(["R" for i in 1:nz], " ") * " )"
+            output_types_wrapper = join(p.output_types, " ")
+
+            # fix new problem bounds
+            print("Computing variable bounds for the reduced problem ...")
+            lower_bound = get_lower_bound(converter, p.lower_bound, p.upper_bound)
+            upper_bound = get_upper_bound(converter, p.lower_bound, p.upper_bound)
+            println("Done")
+
+            # x0 satisfies the equality constraints and the bounds constraints
+            is_x0_evaluated = false
+
+            new_eval_bb = z -> begin
+                if !is_x0_evaluated
+                    is_x0_evaluated = true
+                    return p.eval_bb(x0)
+                end
+                φ_x = convert_to_x(converter, z)
+                if all(p.lower_bound .<= φ_x .<= p.upper_bound)
+                    return p.eval_bb(φ_x)
+                else # in this case, the function is not evaluated
+                    return (false, false, [Inf for i in 1:p.nb_outputs])
+                end
+            end
+
+            create_c_nomad_problem(new_eval_bb, nz, p.nb_outputs,
+                                   lower_bound, upper_bound,
+                                   input_types_wrapper, output_types_wrapper,
+                                   p.options.max_bb_eval)
+        end
+    end
+
+    # 3- set options
     set_nomad_granularity_bb_inputs!(c_nomad_problem, p.granularity)
 
     set_nomad_display_degree!(c_nomad_problem, p.options.display_degree)
@@ -343,11 +431,27 @@ function solve(p::NomadProblem, x0::Vector{Float64})
     set_nomad_nm_search!(c_nomad_problem, p.options.nm_search)
 
     # 4- solve problem
-    result = solve_problem(c_nomad_problem, x0)
+    result = begin
+        if p.A == nothing
+            solve_problem(c_nomad_problem, x0)
+        else
+            z0 = convert_to_z(converter, x0)
+            solve_problem(c_nomad_problem, z0)
+        end
+    end
 
-    sols = (x_best_feas = result[1] ? result[2] : nothing,
-            bbo_best_feas = result[1] ? result[3] : nothing,
-            x_best_inf = result[4] ? result[5] : nothing,
-            bbo_best_inf = result[4] ? result[6] : nothing)
+    sols = begin
+        if p.A == nothing
+        (x_best_feas = result[1] ? result[2] : nothing,
+         bbo_best_feas = result[1] ? result[3] : nothing,
+         x_best_inf = result[4] ? result[5] : nothing,
+         bbo_best_inf = result[4] ? result[6] : nothing)
+        else
+        (x_best_feas = result[1] ? convert_to_x(converter, result[2]) : nothing,
+         bbo_best_feas = result[1] ? result[3] : nothing,
+         x_best_inf = result[4] ? convert_to_x(converter, result[5]) : nothing,
+         bbo_best_inf = result[4] ? result[6] : nothing)
+        end
+    end
     return sols
 end
