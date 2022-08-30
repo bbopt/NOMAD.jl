f2ff3f1ee29c254ab86816b5696207b205110268
salomonl <ludovic.salomon@gerad.ca>
2022-08-30 13:23:01 -0400

    Reactivate all test files

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,16 +3,19 @@ using NOMAD, Test
 include("assertions.jl")
 include("basic_problems.jl")
 
+# VNS search function
+include("CamelVariant.jl")
+
 # HS linear constrained benchmark problems
-# include("HS48.jl")
-# include("HS49.jl")
-# include("HS51.jl")
-# include("HS53.jl")
-# include("HS112.jl")
-# include("HS119.jl")
+include("HS48.jl")
+include("HS49.jl")
+include("HS51.jl")
+include("HS53.jl")
+include("HS112.jl")
+include("HS119.jl")
 
 # Industrial linear constrained benchmark problems
-# include("Dallas.jl")
-# include("Avion2.jl")
-# include("Loadbal.jl")
-# include("ProadPL10.jl")
+include("Dallas.jl")
+include("Avion2.jl")
+include("Loadbal.jl")
+include("ProadPL10.jl")
